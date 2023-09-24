7297dc7968ce4cd69a29c32b3ef2220323a30c33
Impact <manollionut009@gmail.com>
2023-09-24 11:03:28 +0300

    main

---
+++ main.py
@@ -0,0 +1,83 @@
+#var = 10
+
+#array = ["math", "info", "physics", "general culture"]
+#array.append("english")
+#sorted(array)
+#array.sort()
+#print(array)
+
+
+
+#array = [ "Vlad"]
+
+#print(array)
+
+
+
+#int_array = [1, 2, 3, 4, 5,6]
+#float_array = [1.3, -2, 1.54, 3.14, 5.00000333]
+#boole_array = [True, False, True, False]
+#array.append("English")# append is for addingsmth
+#array = sorted(array)#sort is used for sorting
+#array.sort()#sort is used to sort list
+#int_array.sort(reverse=True) #sort(reverse=true) display
+#print(int_array)#print list
+#print(sorted(int_array))#print sorted list
+
+
+#list = []
+#list_lenght = int(input("Enter List Lenght"))
+
+#for i in range(0, list_lenght):
+    #element = int(input(" Enter Element"))
+
+    #list.append(element)
+#else:
+    #print(list)
+    #list.sort()
+    #print(list)
+ #list.sort(reverse=True)
+ #print(list)
+
+
+
+#list = ["12", "56", "2456", "547", "245", "876"]
+#print(list[3])
+
+#if 56 in list:
+    # print(True)
+#else:
+    #print(False)
+
+#list = ["12", "56", "2456", "547", "245", "876"]
+#print(list) #print list
+
+#for i in range(0, len(list)):
+   # print(list[i]) #print list by index
+
+#for e in list:
+    #print(e) #print list by element
+
+#tuple = ("v", "l", "a", "D", "u")
+#print(tuple + ("l", "o", "h"))
+
+#print(tuple[0])
+
+#int_array= [1, 2, 3, 1, 5, 2, 6, 7]
+#int_array= set(int_array)
+
+#set_array = {1, 1, 2, 3, 1, 5, 2, 6, 7}
+#print(set_array)
+#print(int_array)
+
+dex = {"Cod": "Python", "Extend": ".py", "Less": "3"}
+print(dex.get("Cod")) #use key to display value with that
+dex.update({"Less": 3}) #update value where key is Lsss
+print(dex) #print dex
+for i in dex:
+    print(i) # display key
+    print(dex.get(i))#display value with key (i)
+
+
+
+
